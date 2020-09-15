f42ef237648861533a9e41892a47ee37e3bf3018
Yakup TURKAN <siyahas@DESKTOP-07T4E19.localdomain>
2020-09-15 16:47:33 +0300

    added PE 1

---
+++ src/pe_0001.rs
@@ -0,0 +1,23 @@
+/// Finds sum of multiples of 3 and 5 till the given number
+#[allow(dead_code)]
+pub fn multiples_of_3_and_5(i: u32) -> u32 {
+    (0..i).filter(|x| x % 3 == 0 || x % 5 == 0).sum::<u32>()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn till_10() {
+        assert_eq!(23, multiples_of_3_and_5(10))
+    }
+    #[test]
+    fn till_100() {
+        assert_eq!(2318, multiples_of_3_and_5(100))
+    }
+    #[test]
+    fn till_1000() {
+        assert_eq!(233168, multiples_of_3_and_5(1000))
+    }
+}
